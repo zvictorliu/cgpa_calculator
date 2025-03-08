8eda0f027ad5d37b81b5cd4a85947c37fc5a927c
zvictorliu@outlook.com <zvictorliu@outlook.com>
2025-03-08 11:09:33 +0800

    feat: IO from file
    enable default choice

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <vector>
 #include <iomanip>
-
+#include <fstream>
+#include <limits>
 using namespace std;
 
 double calculate_cgpa(vector<double> credit_hours, vector<double> grades){
@@ -20,30 +21,82 @@ double calculate_cgpa(vector<double> credit_hours, vector<double> grades){
     return total_grade_points / total_credit_hours;
 }
 
-int main(){
-
-    string welcome_msg = "\U0001F970 Hello, World!\n"
-                         "Please input your course code, credit hours and grade.\n"
-                         "example: CSE1001 3 95\n"
-                            "Enter EOF to end input\n";
-    cout << welcome_msg;
-
+void input_from_file(vector<string> &course_codes, vector<double> &credit_hourss, vector<double> &grades){
     string course_code;
     double credit_hours;
     double grade;
+
+    string default_file = "data.txt";
+    cout << "Enter the file name " << "[" << default_file << "]: ";
+    string file_name;
+    getline(cin, file_name);
+    if (file_name.empty()){
+        file_name = default_file;
+    }
+    
+    ifstream file(file_name);
+    if (file.is_open()){
+        while (file >> course_code >> credit_hours >> grade){
+            course_codes.push_back(course_code);
+            credit_hourss.push_back(credit_hours);
+            grades.push_back(grade);
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+}
+
+void input_by_hand(vector<string> &course_codes, vector<double> &credit_hourss, vector<double> &grades){
+    string course_code;
+    double credit_hours;
+    double grade;
+    cout << "Enter course code, credit hours and grade (q/quit to exit): " << endl;
+    while (cin >> course_code){
+        if (course_code == "q" || course_code == "quit"){
+            break;
+        }
+        cin >> credit_hours >> grade;
+        course_codes.push_back(course_code);
+        credit_hourss.push_back(credit_hours);
+        grades.push_back(grade);
+    }
+
+    cin.ignore(); // remove the newline character from the input buffer
+}
+
+int main(){
+
+
 
     vector<string> course_codes;
     vector<double> credit_hourss;
     vector<double> grades;
 
+    string welcome_msg = "\U0001F970 Hello, World!\n"
+                         "Do you want to input from file? (y/[n]): ";
+    cout << welcome_msg;
 
-    while (cin >> course_code >> credit_hours >> grade){
-        course_codes.push_back(course_code);
-        credit_hourss.push_back(credit_hours);
-        grades.push_back(grade);
+    string from_file;
+    getline(cin, from_file);
+
+    if (from_file == "y" || from_file == "Y"){
+        input_from_file(course_codes, credit_hourss, grades);
+
+        cout << "Do you want to input more courses? (y/[n]): ";
+        string more_courses;
+        getline(cin, more_courses);
+
+        if (more_courses == "y" || more_courses == "Y"){
+            input_by_hand(course_codes, credit_hourss, grades);
+        }
     }
+    else{
+        input_by_hand(course_codes, credit_hourss, grades);
+    }
+
      
-    cout << "You have input " << course_codes.size() << " courses:\n";
+    cout << "You have input " << course_codes.size() << " courses:" << endl;
 
     for (int i = 0; i < course_codes.size(); i++){
         cout << left << setw(10) << course_codes[i] << 
@@ -54,6 +107,27 @@ int main(){
     double cgpa = calculate_cgpa(credit_hourss, grades);
 
     cout << "Your CGPA is: " << fixed << setprecision(2) << "\033[32m"  << cgpa << "\033[0m" << endl;
+
+    cout << "Do you want save the record to a file? ([y]/n): ";
+
+    string save;
+    getline(cin, save);
+
+    if (save.empty() || save == "y" || save == "Y"){
+        cout << "Enter the file name: ";
+        string file_name;
+        cin >> file_name;
+        ofstream file(file_name);
+        if (file.is_open()){
+            for (int i = 0; i < course_codes.size(); i++){
+                file << course_codes[i] << " " << credit_hourss[i] << " " << grades[i] << "\n";
+            }
+            file.close();
+            cout << "Record saved to " << file_name << endl;
+        } else {
+            cout << "Unable to open file" << endl;
+        }
+    }
 
 
     return 0;
